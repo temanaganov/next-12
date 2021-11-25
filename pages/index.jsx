da2e94a7b483f92e2c68459071edb9693a8480da
Artem Naganov <artemnaganov10@gmail.com>
2021-11-25 16:10:44 +0300

    Remove page component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import Page from '@/components/page';
 import Hello from '@/components/hello';
 
 const Home = () => {
-	return (
-		<Page>
-			<Hello />
-		</Page>
-	);
+	return <Hello />;
 };
 
 export default Home;
